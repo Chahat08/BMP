e78b0288661260f2455c93e8e52d5d9b8aa91d0d
Chahat Kalsi <chahat.ck88@gmail.com>
2024-01-17 08:01:28 -0500

    bmp functs implemented

--- BMP/Bitmap.cpp
+++ BMP/Bitmap.cpp
@@ -1,8 +1,33 @@
+#include <fstream>
 #include "Bitmap.h"
+#include "DIBHeader.h"
+#include "BMPFileHeader.h"
 
 namespace BMP {
 	bool Bitmap::write(std::string filename) {
+		BMP::BMPInfoHeader bmpInfoHeader;
+		BMP::BMPFileHeader bmpFileHeader;
 
+		// setting the deterministic fields in bmpFileHeader
+		bmpFileHeader.fileSize = sizeof(BMPFileHeader) + sizeof(BMPInfoHeader) + sizeof(this);
+		bmpFileHeader.dataOffset = sizeof(BMPFileHeader) + sizeof(BMPInfoHeader);
+
+		// setting the deterministic fields in bmpInfoHeader
+		bmpInfoHeader.biWidth = _width;
+		bmpInfoHeader.biHeight = _height;
+
+		std::fstream file(filename, std::ios::out|std::ios::binary);
+
+		if (file.is_open()) {
+			file.write((char*)&bmpFileHeader, sizeof(bmpFileHeader));
+			file.write((char*)&bmpInfoHeader, sizeof(bmpInfoHeader));
+			file.write((char*)_pPixelData.get(), sizeof(_pPixelData));
+		}
+		else return false;
+
+		file.close();
+
+		return true;
 	}
 
 	void Bitmap::setPixel(int x, int y, uint8_t red, uint8_t blue, uint8_t green) {
--- BMP/Bitmap.h
+++ BMP/Bitmap.h
@@ -9,11 +9,11 @@ namespace BMP {
 	private:
 		int _width{ 0 };
 		int _height{ 0 };
-		std::unique_ptr<uint8_t[]> _pPixel{ nullptr };
+		std::unique_ptr<uint8_t[]> _pPixelData{ nullptr };
 	public:
 		// constructors
 		Bitmap() {};
-		Bitmap(int width, int height) : _width(width), _height(height), _pPixel(new uint8_t[width * height * 3]{}) {};
+		Bitmap(int width, int height) : _width(width), _height(height), _pPixelData(new uint8_t[width * height * 3]{}) {};
 
 		// destructor
 		~Bitmap() {};
